6ddcfb9378de4a1aacabfa19f2eac80d9f7ea538
Timo K. <elveli@gmail.com>
2023-12-01 13:31:42 -0800

    docker hub update

--- lessons/158/upgrade.sh
+++ lessons/158/upgrade.sh
@@ -8,10 +8,10 @@ new_ver=$1
 echo "new version: $new_ver"
 
 # Simulate release of the new docker images
-docker tag nginx:1.23.3 aputra/nginx:$new_ver
+docker tag nginx:1.23.3 elveli113/nginx:$new_ver
 
 # Push new version to dockerhub
-docker push aputra/nginx:$new_ver
+docker push elveli113/nginx:$new_ver
 
 # Create temporary folder
 tmp_dir=$(mktemp -d)
@@ -21,7 +21,7 @@ echo $tmp_dir
 git clone git@github.com:antonputra/lesson-158.git $tmp_dir
 
 # Update image tag
-sed -i '' -e "s/aputra\/nginx:.*/aputra\/nginx:$new_ver/g" $tmp_dir/my-app/1-deployment.yaml
+sed -i '' -e "s/elveli113\/nginx:.*/elveli113\/nginx:$new_ver/g" $tmp_dir/my-app/1-deployment.yaml
 
 # Commit and push
 cd $tmp_dir
